c65dd04c9c1253c09024e8432a502ef34623113b
hager445 <hagetali50@gmail.com>
2025-04-13 14:40:14 +0200

    adding primeNg components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { FormsModule, NgModel } from '@angular/forms';
+
+import { ButtonModule } from 'primeng/button';
+import { CardModule } from 'primeng/card';
+import { CalendarModule } from 'primeng/calendar';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CardModule,
+    ButtonModule , CalendarModule,FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'testvercel';
+  date1:string = '2/3/2020'
+  date2:string = '2/3/2020'
+  date3:string = '2/3/2020'
 }
